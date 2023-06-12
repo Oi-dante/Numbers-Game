138583bbacb9d656ce3034aea6b4055c46b0b400
Gustavo Barboza <gustavobarboza2003@gmail.com>
2023-06-12 20:26:03 -0300

    elements Less and More

--- app/ramdomNumber.js
+++ app/ramdomNumber.js
@@ -1,7 +1,13 @@
-const lessValue = 1
+const lessValue = 0
 const moreValue = 100
 const secretNumber = generateRandomNumber();
 
 function generateRandomNumber() {
   return parseInt(Math.random() * 100)
 }
+
+const elementLessValue = document.getElementById('less-value')
+elementLessValue.innerHTML = lessValue
+
+const elementMoreValue = document.getElementById('mpre-value')
+elementMoreValue.innerHTML = moreValue
