2b8b276659a855185807ad0ff7a66909a6e5f3a6
Gustavo Barboza <gustavobarboza2003@gmail.com>
2023-06-14 21:18:54 -0300

    kick results

--- app/ramdomNumber.js
+++ app/ramdomNumber.js
@@ -3,11 +3,14 @@ const moreValue = 100
 const secretNumber = generateRandomNumber();
 
 function generateRandomNumber() {
-  return parseInt(Math.random() * 100)
+  return parseInt(Math.random() * moreValue + 1)
 }
 
-const elementLessValue = document.getElementById('less-value')
-elementLessValue.innerHTML = lessValue
 
-const elementMoreValue = document.getElementById('mpre-value')
-elementMoreValue.innerHTML = moreValue
+console.log('Secret Number:', secretNumber)
+
+const elementLessValue = document.getElementById('less-value')
+elementLessValue.innerHTML = lessValue;
+
+const elementMoreValue = document.getElementById('more-value')
+elementMoreValue.innerHTML = moreValue;
--- app/voiceRecognition.js
+++ app/voiceRecognition.js
@@ -1,6 +1,23 @@
-window.SpeechRecognition =
-window.SpeechRecognition || window.webkitSpeechRecognition;
+const elementKick = document.getElementById('kick')
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br'
 recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+  kick = e.results[0][0].transcript
+  kickResult(kick)
+  validKick(kick)
+}
+
+function kickResult(kick) {
+  elementKick.innerHTML = `
+    <div>You Say</div>
+    <span class="box">${kick}</span>
+  `
+
+}
