4fc0e89c12e1b09f617df9620617b57d4cdfe0d5
Gustavo Barboza <gustavobarboza2003@gmail.com>
2023-06-15 22:38:58 -0300

    valid kick

--- app/validKick.js
+++ app/validKick.js
@@ -1,1 +1,28 @@
-function validKick(kick)
+function validKick(kick) {
+  const number = +kick
+
+  if (invalidKick(number)) {
+    elementKick.innerHTML += '<div>valor In</div>';
+  }
+
+  if (allowedValue(number)) {
+    elementKick.innerHTML += `<di>
+    Valor in tem que ser entre ${lessValue} e ${moreValue}
+    </div>`;
+  }
+
+  if (number === secretNumber) {
+    document.body.innerHTML = `
+    <h3>Você Acertou</h3>
+    <h2>O Número Secreto era ${secretNumber}</h2>
+    `
+  }
+}
+
+function invalidKick(number) {
+  return Number.isNaN(number)
+}
+
+function allowedValue(number) {
+  return number > moreValue || number < lessValue
+}
